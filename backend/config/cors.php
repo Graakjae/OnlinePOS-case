61dba0dab1efb34fe412916875b008dc8a70e069
Frederik Graakjær <frede.flip@gmail.com>
2025-03-29 16:31:01 +0100

    Add custom CORS middleware and update configuration

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -3,13 +3,13 @@
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
 
-    'allowed_methods' => ['*'],
+    'allowed_methods' => ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
 
     'allowed_origins' => ['https://online-pos-case-ruby.vercel.app'],
 
     'allowed_origins_patterns' => [],
 
-    'allowed_headers' => ['*'],
+    'allowed_headers' => ['Content-Type', 'Authorization', 'X-Requested-With', 'X-XSRF-TOKEN'],
 
     'exposed_headers' => [],
 
